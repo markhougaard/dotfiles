5b133e18a17b80f52881eb3a1d8587ebbfd7390d
Mark Hougaard Jensen <mark@marks.dk>
2020-03-01 00:23:47 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install mas
+brew bundle
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+source ~/.zshrc
+mkdir -p "$HOME/.zsh" && git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+echo "\nfpath+=$HOME/.zsh/pure \nautoload -U promptinit; promptinit \nprompt pure" >> $HOME/.zshrc
+git clone --bare https://github.com/marksdk/dotfiles.git $HOME/.dotfiles
+function dot {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+dot checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dotfiles.";
+    dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dot checkout
+dot config status.showUntrackedFiles no
